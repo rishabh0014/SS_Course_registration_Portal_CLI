aad6f8e0c7b80e6bfe5c2aa289c303810cfc1b0e
rishabh0014 <rishabhteli14@gmail.com>
2023-10-09 18:56:54 +0530

    client->server

--- client.c
+++ client.c
@@ -47,338 +47,192 @@ void hide_password(char *password, int max_length)
     printf("\n");
 }
 
-void writeStudentToFile(struct Student student, const char *filename)
-{
-    int file_fd = open(filename, O_WRONLY | O_APPEND);
-    if (file_fd < 0)
-    {
-        perror("Error opening file");
-        return;
-    }
-
-    char buffer[512];
-    snprintf(buffer, sizeof(buffer), "%s$%s$%s$%d$%s$%s\n",
-             student.login_id,
-             student.password,
-             student.name,
-             student.age,
-             student.email_id,
-             student.address);
-
-    write(file_fd, buffer, strlen(buffer));
-    close(file_fd);
-}
-
-void readStudentsFromFile(const char *filename)
-{
-    int file_fd = open(filename, O_RDONLY);
-    if (file_fd < 0)
-    {
-        perror("Error opening file");
-        return;
-    }
-
-    char buffer[512];
-    ssize_t bytesRead;
-
-    while ((bytesRead = read(file_fd, buffer, sizeof(buffer))) > 0)
-    {
-        char *token = strtok(buffer, ",");
-        struct Student student;
-
-        if (token != NULL)
-        {
-            strncpy(student.login_id, token, sizeof(student.login_id) - 1);
-            token = strtok(NULL, ",");
-        }
-
-        if (token != NULL)
-        {
-            strncpy(student.password, token, sizeof(student.password) - 1);
-            token = strtok(NULL, ",");
-        }
-
-        if (token != NULL)
-        {
-            strncpy(student.name, token, sizeof(student.name) - 1);
-            token = strtok(NULL, ",");
-        }
-
-        if (token != NULL)
-        {
-            student.age = atoi(token);
-            token = strtok(NULL, ",");
-        }
-
-        if (token != NULL)
-        {
-            strncpy(student.email_id, token, sizeof(student.email_id) - 1);
-            token = strtok(NULL, ",");
-        }
-
-        if (token != NULL)
-        {
-            strncpy(student.address, token, sizeof(student.address) - 1);
-        }
-    }
-
-    close(file_fd);
-}
-
-// Student Functionality
-void show_student_menu()
-{
-    printf("1) Enroll to new Courses\n2) Unenroll from already enrolled Courses\n3) View enrolled Courses\n4) Password Change\n5) Exit\n");
-}
-
-// Fauctly Functionality
-void show_faculty_menu()
-{
-    printf("1) Add new Course\n2) Remove offered Course\n3) View enrollments in Courses\n4) Password Change\n5) Exit\n");
-}
-
-// Admin Functionality
-void show_admin_menu()
-{
-    printf("1) Add student\n2) Add Faculty\n3) Activate/Deactivate Student\n4) Update Student/Faculty details\n5) Exit\n");
-}
-
 int main()
 {
-
-    // DO NOT TOUCH CODE START
-    int client_socket;
+    int client_socket, num;
     struct sockaddr_in server_addr;
     int auth_status, change_pass_status;
-    char username[50], password[50], curr_password[50], new_password[50];
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket < 0)
-    {
-        perror("Error in socket creation");
-        exit(1);
-    }
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = PORT;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("Error in connection");
-        exit(1);
-    }
+    char username[50], password[50], curr_password[50], new_password[50], welcome[100], select_role[100], en_username[20], en_password[20];
 
-    printf("Hello! Welcome to the Course Registration Portal\n");
-    int num;
-
-    // choose role
-    while (1)
+    // socket
     {
-        printf("Log in As:\n 1) Student\n 2) Faculty\n 3) Admin\n 4) Exit\n");
-        printf("Enter your choice: ");
+        client_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (client_socket < 0)
+        {
+            perror("Error in socket creation");
+            exit(1);
+        }
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = PORT;
+        server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        {
+            perror("Error in connection");
+            exit(1);
+        }
+    }
+    // welcome
+    {
+        recv(client_socket, welcome, sizeof(welcome), 0);
+        printf("%s", welcome);
+        recv(client_socket, select_role, sizeof(select_role), 0);
+        printf("%s", select_role);
+    }
+    // Choice
+    {
         scanf("%d", &num);
-        if (num < 1 || 4 < num)
-        {
-            printf("Invalid Choice!! Try Again");
-        }
-        else
-        {
-            break;
-        }
+        send(client_socket, &num, sizeof(int), 0);
     }
+    // Exit
     if (num == 4)
     {
-        printf("Bye Bye!!\n");
-        printf("Exiting...!!\n");
+        char exit_[30];
+        recv(client_socket, exit_, sizeof(exit_), 0);
+        printf("%s", exit_);
         exit(0);
     }
-
-    printf("Enter username: ");
-    scanf("%s", username);
-    getchar();
-    printf("Enter password: ");
-    hide_password(password, sizeof(password));
-
-    send(client_socket, &num, sizeof(int), 0);
-    send(client_socket, username, sizeof(username), 0);
-    send(client_socket, password, sizeof(password), 0);
-    recv(client_socket, &auth_status, sizeof(int), 0);
-
-    if (auth_status == 1)
-    {
-        printf("Authentication successful!\n");
-        printf("Hello, %s! Welcome..\n", username);
-        // DO NOT TOUCH CODE END
-        // student logic
-        if (num == 1)
-        {
-            int choice;
-            while (1)
-            {
-                show_student_menu();
-                scanf("%d", &choice);
-                if (choice < 1 || 5 < choice)
-                {
-                    printf("Invalid choice.. Try Again...\n");
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            if (choice == 1)
-            {
-                // Enroll to new Courses
-            }
-            else if (choice == 2)
-            {
-                // Unenroll from already enrolled Courses
-            }
-            else if (choice == 3)
-            {
-                // View enrolled Courses
-            }
-            else if (choice == 4)
-            {
-                // Password Change
-                printf("Enter Current Password: ");
-                scanf("%s", curr_password);
-                printf("Enter New Password: ");
-                scanf("%s", new_password);
-                send(client_socket, &choice, sizeof(int), 0);
-                send(client_socket, curr_password, sizeof(curr_password), 0);
-                send(client_socket, new_password, sizeof(new_password), 0);
-                recv(client_socket, &change_pass_status, sizeof(int), 0);
-                if (change_pass_status == 1)
-                {
-                    printf("succ");
-                }
-                else
-                {
-                    printf("again");
-                }
-            }
-            else if (choice == 5)
-            {
-                // Exit
-                printf("Signing Out... BYE BYE!!");
-                return 0;
-            }
-        }
-        // faculty logic
-        else if (num == 2)
-        {
-            int choice;
-            while (1)
-            {
-                show_faculty_menu();
-                scanf("%d", &choice);
-                if (choice < 1 || 5 < choice)
-                {
-                    printf("Invalid choice.. Try Again...\n");
-                }
-                else
-                {
-                    break;
-                }
-            }
-            if (choice == 1)
-            {
-                // Add new Course
-            }
-            else if (choice == 2)
-            {
-                // Remove offered Course
-            }
-            else if (choice == 3)
-            {
-                // View enrollments in Courses
-            }
-            else if (choice == 4)
-            {
-                // Password Change
-            }
-            else if (choice == 5)
-            {
-                // Exit
-                printf("Signing Out... BYE BYE!!");
-                return 0;
-            }
-        }
-        // admin logic
-        else if (num == 3)
-        {
-            int choice;
-            while (1)
-            {
-                show_admin_menu();
-                scanf("%d", &choice);
-                if (choice < 1 || 5 < choice)
-                {
-                    printf("Invalid choice.. Try Again...\n");
-                }
-                else
-                {
-                    break;
-                }
-            }
-            // Add student
-            if (choice == 1)
-            {
-                struct Student student_info;
-                getchar();
-                printf("Enter Roll No/Log in ID: ");
-                scanf("%[^\n]", student_info.login_id);
-                getchar();
-                printf("Enter Password: ");
-                scanf("%[^\n]", student_info.password);
-                getchar();
-                printf("Enter Name: ");
-                scanf("%[^\n]", student_info.name);
-                getchar();
-                printf("Enter age: ");
-                scanf("%d", &student_info.age);
-                getchar();
-                printf("Enter email ID: ");
-                scanf("%[^\n]", student_info.email_id);
-                getchar();
-                printf("Enter Address: ");
-                scanf("%[^\n]", student_info.address);
-                getchar();
-                // Write student data to the file
-                // writeStudentToFile(student_info, "data/students_data/student_data.txt");
-
-                // int add_std = 1;
-                send(client_socket,&choice,sizeof(int),0);
-                
-                char buffer[sizeof(struct Student)];
-                memcpy(buffer, &student_info, sizeof(struct Student));
-                
-                send(client_socket, buffer, sizeof(struct Student), 0);
-
-            }
-            // Add Faculty
-            else if (choice == 2)
-            {
-            }
-            // Activate/Deactivate Student
-            else if (choice == 3)
-            {
-            }
-            // Update Student/Faculty details
-            else if (choice == 4)
-            {
-            }
-            // Exit
-            else if (choice == 5)
-            {
-                printf("Signing Out... BYE BYE!!");
-                return 0;
-            }
-        }
-    }
+    // Student Faculty Admin
     else
     {
-        printf("Authentication failed.\n");
+        // Input from User (username and password)
+        {
+            recv(client_socket, en_username, sizeof(en_username), 0);
+            printf("%s", en_username);
+            scanf("%s", username);
+            getchar();
+            recv(client_socket, en_password, sizeof(en_password), 0);
+            printf("%s", en_password);
+            hide_password(password, sizeof(password));
+            send(client_socket, username, sizeof(username), 0);
+            send(client_socket, password, sizeof(password), 0);
+        }
+
+        // student
+        if (num == 1)
+        {
+            recv(client_socket, &auth_status, sizeof(int), 0);
+            if (auth_status == 1)
+            {
+                printf("Authentication successful!\n");
+                printf("Hello, %s! Welcome..\n", username);
+                char student_menu[200];
+                recv(client_socket, student_menu, sizeof(student_menu), 0);
+                printf("%s", student_menu);
+                int choice;
+                scanf("%d", &choice);
+                send(client_socket, &choice, sizeof(int), 0);
+
+                // Enroll to new Courses
+                if (choice == 1)
+                {
+                }
+                // Unenroll from already enrolled Courses
+                else if (choice == 2)
+                {
+                }
+                // View enrolled Courses
+                else if (choice == 3)
+                {
+                }
+                // Password Change
+                else if (choice == 4)
+                {
+                    // printf("Enter Current Password: ");
+                    // scanf("%s", curr_password);
+                    // printf("Enter New Password: ");
+                    // scanf("%s", new_password);
+                    // send(client_socket, &choice, sizeof(int), 0);
+                    // send(client_socket, curr_password, sizeof(curr_password), 0);
+                    // send(client_socket, new_password, sizeof(new_password), 0);
+                    // recv(client_socket, &change_pass_status, sizeof(int), 0);
+                    // if (change_pass_status == 1)
+                    // {
+                    //     printf("succ");
+                    // }
+                    // else
+                    // {
+                    //     printf("again");
+                    // }
+                }
+                // Exit
+                else if (choice == 5)
+                {
+                    printf("Signing Out... BYE BYE!!");
+                    return 0;
+                }
+            }
+            else
+            {
+                printf("Authentication failed.\n");
+            }
+        }
+        // faculty
+        else if (num == 2)
+        {
+        }
+        // admin
+        else if (num == 3)
+        {
+            recv(client_socket, &auth_status, sizeof(int), 0);
+            if (auth_status == 1)
+            {
+                printf("Authentication successful!\n");
+                printf("Hello, %s! Welcome..\n", username);
+                char admin_menu[200];
+                recv(client_socket, admin_menu, sizeof(admin_menu), 0);
+                printf("%s", admin_menu);
+                int choice;
+                scanf("%d", &choice);
+                send(client_socket, &choice, sizeof(choice), 0);
+                if (choice == 1)
+                {
+                    struct Student student_info;
+                    // Taking student Info
+                    {
+                        getchar();
+                        printf("Enter Roll No/Log in ID: ");
+                        scanf("%[^\n]", student_info.login_id);
+                        getchar();
+                        printf("Enter Password: ");
+                        scanf("%[^\n]", student_info.password);
+                        getchar();
+                        printf("Enter Name: ");
+                        scanf("%[^\n]", student_info.name);
+                        getchar();
+                        printf("Enter age: ");
+                        scanf("%d", &student_info.age);
+                        getchar();
+                        printf("Enter email ID: ");
+                        scanf("%[^\n]", student_info.email_id);
+                        getchar();
+                        printf("Enter Address: ");
+                        scanf("%[^\n]", student_info.address);
+                        getchar();
+                        printf("%s,%s,%s,%d,%s,%s\n", student_info.login_id, student_info.password, student_info.name, student_info.age, student_info.email_id, student_info.address);
+                        // send(client_socket, &choice, sizeof(int), 0);
+                    }
+
+                    char buffer[sizeof(struct Student)];
+                    memcpy(buffer, &student_info, sizeof(struct Student));
+
+                    send(client_socket, buffer, sizeof(struct Student), 0);
+                }
+                else if (choice == 2)
+                {
+                }
+                else if (choice == 3)
+                {
+                }
+                else if (choice == 4)
+                {
+                }
+            }
+            else
+            {
+                printf("Authentication failed.\n");
+            }
+        }
     }
+
     return 0;
 }
--- server.c
+++ server.c
@@ -27,7 +27,7 @@ struct Student
     char address[100];
 };
 
-void writeStudentToFile(struct Student student, const char *filename)
+void write_student_data_to_file(struct Student student, const char *filename)
 {
     int file_fd = open(filename, O_WRONLY | O_APPEND);
     if (file_fd < 0)
@@ -49,7 +49,22 @@ void writeStudentToFile(struct Student student, const char *filename)
     close(file_fd);
 }
 
-int readUsersFromFile(struct User users[], const char *filename)
+void write_stident_log_in_data_to_file(struct Student student, const char *filename)
+{
+    int file_fd = open(filename, O_WRONLY | O_APPEND);
+    if (file_fd < 0)
+    {
+        perror("Error opening File");
+        return;
+    }
+
+    char buffer[120];
+    snprintf(buffer, sizeof(buffer), "%s$%s\n", student.login_id, student.password);
+    write(file_fd, buffer, strlen(buffer));
+    close(file_fd);
+}
+
+int readUsersFromFile(const char *username, const char *password, const char *filename)
 {
     int file_fd = open(filename, O_RDONLY);
     if (file_fd < 0)
@@ -57,26 +72,37 @@ int readUsersFromFile(struct User users[], const char *filename)
         perror("Error opening file");
         return 0;
     }
-    int count = 0;
-    char buffer[1024];
+
+    char buffer[120];
     ssize_t bytesRead;
+    int auth_status = 0;
+
     while ((bytesRead = read(file_fd, buffer, sizeof(buffer))) > 0)
     {
-        char *token = strtok(buffer, " \n");
-        while (token != NULL)
+        char *token = strtok(buffer, "$");
+        char stored_username[50], stored_password[50];
+
+        if (token != NULL)
         {
-            strcpy(users[count].username, token);
-            token = strtok(NULL, " \n");
-            if (token != NULL)
-            {
-                strcpy(users[count].password, token);
-                token = strtok(NULL, " \n");
-                count++;
-            }
+            strncpy(stored_username, token, sizeof(stored_username) - 1);
+            token = strtok(NULL, "$");
+        }
+        if (token != NULL)
+        {
+            strncpy(stored_password, token, sizeof(stored_password) - 1);
+            token = strtok(NULL, "$");
+        }
+
+        // printf("%s=>%s||%s=>%s\n",stored_username,username,stored_password,password);
+        if (strcmp(stored_username, username) == 0 && strcmp(stored_password, password) == 0)
+        {
+            auth_status = 1;
+            break;
         }
     }
+
     close(file_fd);
-    return count;
+    return auth_status;
 }
 
 int changePassword(struct User users[], const char *username, const char *current_password, const char *new_password)
@@ -102,117 +128,131 @@ void *handle_client(void *arg)
     char curr_password[50], new_password[50];
     int num, choice;
     int auth_status = 0, change_pass_status = 0;
-    struct User users[MAX_CLIENTS];
+    // struct User users[MAX_CLIENTS];
     int users_count;
 
-    read(client_socket, &num, sizeof(int));
+    char welcome[100] = "Hello! Welcome to the Course Registration Portal\n";
+    char select_role[100] = "Log in As:\n 1) Student\n 2) Faculty\n 3) Admin\n 4) Exit\nEnter your Choice: ";
 
-    read(client_socket, username, sizeof(username));
-    read(client_socket, password, sizeof(password));
-    if (num == 1)
-    { // student Logic
-        users_count = readUsersFromFile(users, "data/students_data/student_log_in.txt");
-        for (int i = 0; i < users_count; i++)
+    send(client_socket, welcome, sizeof(welcome), 0);
+    send(client_socket, select_role, sizeof(select_role), 0);
+    recv(client_socket, &num, sizeof(num), 0);
+
+    if (num == 4)
+    {
+        char exit_[30] = "Bye Bye!!\nExiting...!!\n";
+        send(client_socket, exit_, sizeof(exit_), 0);
+    }
+    else
+    {   
+        char en_username[20] = "Enter username: ";
+        char en_password[20] = "Enter password: ";
+        send(client_socket, en_username, sizeof(en_username), 0);
+        send(client_socket, en_password, sizeof(en_password), 0);
+        read(client_socket, username, sizeof(username));
+        read(client_socket, password, sizeof(password));
+        // student Logic
+        if (num == 1)
         {
-            if (strcmp(username, users[i].username) == 0 && strcmp(password, users[i].password) == 0)
+            if (readUsersFromFile(username, password, "data/students_data/student_log_in.txt"))
             {
                 auth_status = 1;
-                break;
             }
-        }
-        send(client_socket, &auth_status, sizeof(int), 0);
-
-        read(client_socket, &choice, sizeof(int));
-        if (choice == 1)
-        {
-            // Enroll to new Courses
-        }
-        else if (choice == 2)
-        {
-            // Unenroll from already enrolled Courses
-        }
-        else if (choice == 3)
-        {
-            // View enrolled Courses
-        }
-        else if (choice == 4)
-        {
-            // Password Change
-            read(client_socket, curr_password, sizeof(curr_password));
-            read(client_socket, new_password, sizeof(new_password));
-
-            if (changePassword(users, username, curr_password, new_password))
+            send(client_socket, &auth_status, sizeof(int), 0);
+            if (auth_status == 1)
             {
-                FILE *file = fopen("data/students_data/student_log_in.txt", "w");
-                if (file != NULL)
-                {
-                    for (int j = 0; j < users_count; j++)
-                    {
-                        fprintf(file, "%s %s\n", users[j].username, users[j].password);
-                    }
-                    fclose(file);
-                }
-                else
-                {
-                    perror("Error opening user data file for update");
-                }
-                change_pass_status = 1;
-            }
-            send(client_socket, &change_pass_status, sizeof(int), 0);
-        }
-    }
+                char student_menu[200] = "1) Enroll to new Courses\n2) Unenroll from already enrolled Courses\n3) View enrolled Courses\n4) Password Change\n5) Exit\n";
+                send(client_socket, student_menu, sizeof(student_menu), 0);
+                recv(client_socket, &choice, sizeof(int), 0);
 
-    // LAST
-    else if (num == 2)
-    {
+                if (choice == 1)
+                {
+                    // Enroll to new Courses
+                }
+                else if (choice == 2)
+                {
+                    // Unenroll from already enrolled Courses
+                }
+                else if (choice == 3)
+                {
+                    // View enrolled Courses
+                }
+                else if (choice == 4)
+                {
+                    // Password Change
+                    // read(client_socket, curr_password, sizeof(curr_password));
+                    // read(client_socket, new_password, sizeof(new_password));
+                    // if (changePassword(users, username, curr_password, new_password))
+                    // {
+                    //     FILE *file = fopen("data/students_data/student_log_in.txt", "w");
+                    //     if (file != NULL)
+                    //     {
+                    //         for (int j = 0; j < users_count; j++)
+                    //         {
+                    //             fprintf(file, "%s %s\n", users[j].username, users[j].password);
+                    //         }
+                    //         fclose(file);
+                    //     }
+                    //     else
+                    //     {
+                    //         perror("Error opening user data file for update");
+                    //     }
+                    //     change_pass_status = 1;
+                    // }
+                    // send(client_socket, &change_pass_status, sizeof(int), 0);
+                }
+            }
+            else
+            {
+                // authentication fail
+            }
+        }
         // faculty logic
-        users_count = readUsersFromFile(users, "data/faculties_data/faculties_log_in.txt");
-        for (int i = 0; i < users_count; i++)
+        else if (num == 2)
         {
-            if (strcmp(username, users[i].username) == 0 && strcmp(password, users[i].password) == 0)
+            if (readUsersFromFile(username, password, "data/faculties_data/faculties_log_in.txt"))
             {
                 auth_status = 1;
-                break;
             }
+            send(client_socket, &auth_status, sizeof(int), 0);
         }
-        send(client_socket, &auth_status, sizeof(int), 0);
-    }
-    else if (num == 3)
-    {
         // admin logic
-        users_count = readUsersFromFile(users, "data/admins_data/admin_log_in.txt");
-        for (int i = 0; i < users_count; i++)
+        else if (num == 3)
         {
-            if (strcmp(username, users[i].username) == 0 && strcmp(password, users[i].password) == 0)
+            if (readUsersFromFile(username, password, "data/admins_data/admin_log_in.txt"))
             {
                 auth_status = 1;
-                break;
+            }
+            send(client_socket, &auth_status, sizeof(int), 0);
+
+            char admin_menu[200] = "1) Add student\n2) Add Faculty\n3) Activate/Deactivate Student\n4) Update Student/Faculty details\n5) Exit\n";
+            send(client_socket, admin_menu, sizeof(admin_menu), 0);
+
+            read(client_socket, &choice, sizeof(int));
+
+            if (choice == 1)
+            {
+                // Add Student
+                printf("HERE\n");
+                char buffer[sizeof(struct Student)];
+                recv(client_socket, buffer, sizeof(struct Student), 0);
+                struct Student student_info;
+                memcpy(&student_info, buffer, sizeof(struct Student));
+                printf("%s,%s,%s,%d,%s,%s\n", student_info.login_id, student_info.password, student_info.name, student_info.age, student_info.email_id, student_info.address);
+                write_student_data_to_file(student_info, "data/students_data/student_data.txt");
+                write_stident_log_in_data_to_file(student_info, "data/students_data/student_log_in.txt");
+            }
+            else if (choice == 2)
+            {
+            }
+            else if (choice == 3)
+            {
+            }
+            else if (choice == 4)
+            {
             }
         }
-        send(client_socket, &auth_status, sizeof(int), 0);
-
-        read(client_socket, &choice, sizeof(int));
-        if (choice == 1)
-        {
-            // Add Student
-            char buffer[sizeof(struct Student)];
-            recv(client_socket, buffer, sizeof(struct Student), 0);
-            struct Student student_info;
-            memcpy(&student_info, buffer, sizeof(struct Student));
-            printf("%s",student_info.name);
-            writeStudentToFile(student_info, "data/students_data/student_data.txt");
-        }
-        else if (choice == 2)
-        {
-        }
-        else if (choice == 3)
-        {
-        }
-        else if (choice == 4)
-        {
-        }
     }
-
     close(client_socket);
     clients_count--;
     pthread_exit(NULL);
